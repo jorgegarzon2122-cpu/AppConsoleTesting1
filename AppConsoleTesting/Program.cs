c38fe0a9e606346d05be96390dbe15d30ee3ebe5
jorgegarzon2122-cpu <jorgegarzon2122@gmail.com>
2026-02-09 19:36:47 -0500

    Agregar archivos de proyecto.

---
+++ AppConsoleTesting/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Esto es una prueba de git v1");
+
+Console.WriteLine("Esto es una prueba de git v2");
+
+Console.WriteLine("Jorge Luis Garzon Parada");
